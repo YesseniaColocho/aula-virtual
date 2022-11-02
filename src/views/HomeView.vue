542f268a1923f97237d30f115007ce9ac6ed65ec
YesseniaColocho <Colocho.yessenia@gmail.com>
2022-11-02 11:52:57 +0100

    Limpieza de proyecto por defecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    Hola! Soy la View
   </main>
 </template>
+
+<script setup>
+//Aqui van la importaciones de componentes
+</script>
