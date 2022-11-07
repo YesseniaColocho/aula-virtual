3a4b0ed29acfc545edf716b5ffd72ce0069ac266
YesseniaColocho <Colocho.yessenia@gmail.com>
2022-11-07 19:08:33 +0100

    AV-7: Pagina de entrega final

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ComposicionView from '../views/ComposicionView.vue'
+import EntregaView from '../views/EntregaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/composicion',
       name: 'composicion',
       component: ComposicionView
+    },
+    {
+    path: '/entrega',
+    name: 'entrega',
+    component: EntregaView
     }
   ]
 })
