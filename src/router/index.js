245e48331cc6b385a410a60141fbd522e31e0bec
YesseniaColocho <Colocho.yessenia@gmail.com>
2022-11-08 15:18:13 +0100

    AV-8: Pagina Mensajeria

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ComposicionView from '../views/ComposicionView.vue'
 import EntregaView from '../views/EntregaView.vue'
+import MensajeriaView from '../views/MensajeriaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
     path: '/entrega',
     name: 'entrega',
     component: EntregaView
+    },
+    {
+      path: '/mensajeria',
+      name: 'mensajeria',
+      component: MensajeriaView
     }
   ]
 })
